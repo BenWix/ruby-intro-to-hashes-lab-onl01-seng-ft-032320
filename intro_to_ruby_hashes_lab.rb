716ac6c44430b5aa512ea094880ac78aa89b9b65
Benjamin Wichser <benwichser@gmail.com>
2020-03-25 20:23:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,8 @@ def monopoly_with_third_tier
 	monopoly[:railroads][:names] = {
 	  reading_railroad: {},
 	  pennsylvania_railroad: {},
-	  
+	  b_and_o_railroad: {},
+	  shortline: {}
 	monopoly
 end
 
